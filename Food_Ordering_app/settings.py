852b2750c8e8d23e71a22183eefd36c206e7e60b
Harshitha1412 <147846515+Harshitha1412@users.noreply.github.com>
2024-04-29 19:43:27 +0530

    Update settings.py

--- Food_Ordering_app/settings.py
+++ Food_Ordering_app/settings.py
@@ -74,19 +74,6 @@ WSGI_APPLICATION = 'Food_Ordering_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql',
-#          'NAME': 'Quicker',
-#          'USER': 'postgres',
-#          'PASSWORD': '12345',
-#          'HOST': 'localhost',
-#          'PORT': '5432',
-#  }
-#  }
-
-
-
 
 DATABASES = {
     'default': {
